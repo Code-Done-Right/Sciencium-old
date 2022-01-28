28810b6aaa957101d819a0294b781bee0bd9aeff
Science Done Right <sciencedoneright5@gmail.com>
2022-01-28 20:48:10 +0530

    nothing much just added the navbar import

---
+++ sciencium/src/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './home.css';
+import { Navbar as navbar } from './components/navbar.jsx';
+
+function App () {
+    return (
+        <div className='App-main'>
+            <h1 className='header'>Hello World!</h1>
+        </div>
+    );
+}
+
+export default App;
