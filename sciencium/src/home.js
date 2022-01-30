514c8e74203ea94f5d4301538dbd5553cbbfa967
Science Done Right <sciencedoneright5@gmail.com>
2022-01-30 11:47:35 +0530

    Added main text that will show up on the front page, deleted previous code (full of bugs)

--- sciencium/src/home.js
+++ sciencium/src/home.js
@@ -1,13 +1,32 @@
+// Imports
 import React from 'react';
 import './home.css';
-import { Navbar as navbar } from './components/navbar.jsx';
 
-function App () {
+// Import components from component folder, add to HomePage function 
+function HomePage () {
     return (
-        <div className='App-main'>
-            <h1 className='header'>Hello World!</h1>
-        </div>
+        <>
+            {/* bgImageContainer is supposed to change the background image for a set time interval. Will be implemented soon. */}
+            
+            {/*
+            <div class="bgImageContainer">
+                <img class="current" src="old.png"/>
+                <img class="next" src="new.png"/>
+            </div>
+            */}
+
+            <div className='meta-div'>
+                <div>
+                    <div className="text">
+                        <h1 className='header-text'>
+                        Welcome to <br/> Sciencium, <br/> the best <br/> learning webpage!
+                        </h1>
+                        Start reading our <br/> website material to <br/> learn more about science!
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
-export default App;
+export default HomePage;
