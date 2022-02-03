bab3b7a189dd8e51afa8082e943eb1ce35047840
Science Done Right <sciencedoneright5@gmail.com>
2022-02-03 18:50:51 +0530

    Added login file to the website

---
+++ src/pages/Login.js
@@ -0,0 +1,20 @@
+// Imports
+import React from 'react';
+import '../styles/pages/Login.css';
+
+function Login () {
+    return (
+        <div className='login-page'>
+            <div className='hero-wrapper'>
+                <h1 className='ff-primary'>
+                    Login
+                </h1>
+				<p className='description ff-secondary'>
+					mhmadidas
+				</p>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
