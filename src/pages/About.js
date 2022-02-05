819650533bf82c452484b20798c67a705e051aea
simonGreenwood <simonrobertgreenwood@gmail.com>
2022-02-05 22:11:36 +0000

    Did work on the about and the courses pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,21 @@
 import Nav from "../components/Nav"
-
+import "../styles/pages/About.css"
 const About = () => {
+    
     return (
-        <div>
-            <Nav/>
-            About Page
+        <div className="home-page">
+            <div className="hero-wrapper">
+            <div className="hero-left">
+                <h1 className="welcome ff-primary">
+                About Sciencium
+                </h1>
+            </div>
+            <div className="hero-right about ff-secondary">
+              <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non mauris in quam ullamcorper tincidunt elementum laoreet lorem. Sed posuere massa a nibh viverra, sed cursus nisi commodo. Vestibulum molestie sit amet augue in interdum. Donec pulvinar velit turpis, malesuada viverra nibh suscipit ac. Nullam gravida sapien et elit blandit aliquam. Vestibulum varius magna eu nisi finibus, nec pellentesque dui feugiat. Nullam a congue purus. Morbi id urna at ex rhoncus porttitor id consequat nulla. Fusce sit amet suscipit ipsum. Maecenas turpis tortor, scelerisque nec aliquam nec, ornare aliquet nulla. Nullam scelerisque lectus iaculis, commodo est sit amet, finibus ante.</p>
+              <p>Aenean eros lorem, ultrices at tortor et, ornare laoreet arcu. Ut aliquet, tellus non consequat elementum, dui urna cursus diam, eu pulvinar ante risus id est. Aenean tortor augue, consectetur non ex et, viverra lobortis nibh. Morbi imperdiet luctus nisi, nec mattis diam elementum nec. Nunc sit amet aliquet turpis. Donec ultrices felis eu felis faucibus suscipit. Vivamus justo ligula, placerat ac mattis eu, sagittis vitae arcu. Pellentesque arcu ipsum, bibendum eu urna et, hendrerit finibus tellus. Aenean sed metus dictum nulla faucibus dignissim. Sed sagittis ipsum augue, sed sodales justo interdum vitae.</p>
+              <p>Quisque vel ipsum eget lectus fermentum feugiat. Aenean rhoncus erat at cursus posuere. Aliquam erat volutpat. Ut lobortis urna vel blandit malesuada. Mauris arcu odio, tincidunt in pretium vitae, commodo id nibh. Vivamus non nisl in libero sagittis hendrerit eu vitae turpis. Cras viverra leo id metus varius, vel ullamcorper odio aliquam. Donec dignissim ante sit amet sapien consequat, eget eleifend sem finibus. Donec placerat urna efficitur purus pretium scelerisque. Praesent sodales laoreet enim eget euismod. Fusce a ornare sem. </p>
+            </div>
+            </div>
         </div>
     )
 }
--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,11 +1,16 @@
-import Nav from "../components/Nav"
+import React from 'react';
+import '../styles/pages/Courses.css';
 
-const Courses = () => {
+function Courses () {
     return (
-        <div>
-            <Nav/>
-            Courses Page
+        <div className='courses-page'>
+            <div className='header'>
+                <h1 className='header-title ff-primary'>Check out our courses!</h1>
+                <p className='header-text ff-secondary'>Here are all of our science courses:</p>
+                <p className='header-text ff-secondary'>This feature is currently in development, A separate navbar will be added for this.</p>
+            </div>
         </div>
-    )
+    );
 }
-export default Courses
+
+export default Courses;
