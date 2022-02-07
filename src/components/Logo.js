ad3743f9772f729f54d8032e5801c6224c5d9573
Pranjal Roy <76814723+PranjalRoy123@users.noreply.github.com>
2022-02-07 10:03:40 +0530

    redesign

---
+++ src/components/Logo.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../styles/components/Logo.css";
+
+function Logo() {
+  return (
+    <div className="logo ff-primary">
+      <span className="logo-letter">S</span>
+      <span className="logo-letter">C</span>
+      <span className="logo-letter">I</span>
+      <span className="logo-letter">E</span>
+      <span className="logo-letter">N</span>
+      <span className="logo-letter">C</span>
+      <span className="logo-letter">I</span>
+      <span className="logo-letter">U</span>
+      <span className="logo-letter">M</span>
+    </div>
+  );
+}
+
+export default Logo;
