e62b79670c292f2c747ca9371fc0a45c01dd12e7
Science Done Right <81095108+Code-Done-Right@users.noreply.github.com>
2022-02-07 17:20:12 +0530

    updated dependencies in yarn.lock

---
+++ src/components/headers.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function PrimaryHeader () {
+    return (
+        <h1 className='ff-primary'>
+
+        </h1>
+    );
+}
+
+function PrimaryHeader () {
+    return (
+        <h1 className='ff-secondary'>
+            
+        </h1>
+    );
+}
