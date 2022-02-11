69a3a10d548cec5214229da4089675eb76f6e59a
Science Done Right <81095108+Code-Done-Right@users.noreply.github.com>
2022-02-11 18:51:46 +0530

    Added the courses page beginning, made progress on the navigation sidebar too

--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import '../styles/pages/Courses.css';
+import Sidebar from '../components/Sidebar'
+
+// Subpages
+import  GetStartedPage  from './pages_courses/get_started';
 
 function Courses () {
     return (
         <div className='courses-page'>
+            <Sidebar />
+            <GetStartedPage />
         </div>
     );
 }
