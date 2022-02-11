d96ee2ba7cd39d44a4278a7d22c8c9c2ab645f27
Science Done Right <81095108+Code-Done-Right@users.noreply.github.com>
2022-02-11 18:51:05 +0530

    Small changes to about page, still a WIP

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../styles/pages/About.css';
+
+function About () {
+    return(
+        <div className='about-page'>
+            <h1 className='about-text ff-primary'>
+                Hi there! We are the developers of Sciencium, trying to make people love science more than ever. It's good to see you use this website, and thanks!
+            </h1>
+            <div className='contributors ff-secondary'>
+                {/* 
+                    This section is meant to show the contributor's PFP, info and more.
+                    Please add your info here if you're a collaborator <3
+                */}
+            </div>
+        </div>
+    );
+}
+
+export default About;
